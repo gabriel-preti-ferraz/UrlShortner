84563ae460a88f5211c03f555e6d54dff51cff75
Gabriel Preti Ferraz <125829214+gabriel-preti-ferraz@users.noreply.github.com>
2025-12-31 11:37:15 -0300

    feat: return short link on res

--- backend/index.js
+++ backend/index.js
@@ -7,16 +7,20 @@ import client from "./db.js"
 
 app.use(express.json())
 app.use(cors())
-app.listen(process.env.API_PORT, () => console.log(`Server is running on ${process.env.API_URL}:${process.env.API_PORT}`))
+app.listen(process.env.API_PORT, () => console.log(`Server is running on PORT ${process.env.API_PORT}`))
 
 app.post("/short", async (req, res) => {
     try {
         const { originalUrl } = req.body
+        if (!originalUrl) {
+            return res.status(400).json({ error: "Missing URL"})
+        }
+
         const shortId = Math.random().toString(36).substring(2, 8)
         const result = await client.query(
             "INSERT INTO urls (original_url, short_url) VALUES($1, $2) RETURNING short_url", [originalUrl, shortId]
         )
-        res.status(201).json(result.rows)
+        res.status(201).json({shortUrl: `${process.env.SITE_URL}/${result.rows[0].short_url}`})
     } catch (err) {
         console.log(err.message)
         res.status(500).send("Server error")
