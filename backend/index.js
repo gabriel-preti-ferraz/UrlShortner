5d3e64133da716bd0d269698a53ab9c91a862a6e
Gabriel Preti Ferraz <125829214+gabriel-preti-ferraz@users.noreply.github.com>
2025-12-31 11:56:33 -0300

    feat: redirect

--- backend/index.js
+++ backend/index.js
@@ -19,9 +19,30 @@ app.post("/short", async (req, res) => {
 
         const shortId = nanoid(6)
         const result = await client.query(
-            "INSERT INTO urls (original_url, short_url) VALUES($1, $2) RETURNING short_url", [originalUrl, shortId]
+            "INSERT INTO urls (original_url, short_url) VALUES($1, $2) RETURNING short_url", 
+            [originalUrl, shortId]
         )
         res.status(201).json({shortUrl: `${process.env.SITE_URL}/${result.rows[0].short_url}`})
+    } catch (err) {
+        console.log(err.message)
+        res.status(500).send("Server error")
+    }
+})
+
+app.get("/redirect/:shortId", async (req, res) => {
+    try {
+        const {shortId} = req.params
+        const result = await client.query(
+            "SELECT original_url FROM urls WHERE short_url ILIKE $1",
+            [shortId]
+        )
+
+        if (!result.rows) {
+            return res.status(404).json({error: "Link does not exist"})
+        }
+
+        const originalUrl = result.rows[0].original_url
+        res.redirect(originalUrl)
     } catch (err) {
         console.log(err.message)
         res.status(500).send("Server error")
