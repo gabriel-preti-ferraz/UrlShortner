69f960a59539204ed83ac8bc14e3b69b201c3d70
Gabriel Preti Ferraz <125829214+gabriel-preti-ferraz@users.noreply.github.com>
2025-12-31 11:20:34 -0300

    feat: db connection

---
+++ backend/db.js
@@ -0,0 +1,15 @@
+import { Client } from "pg"
+import dotenv from "dotenv"
+dotenv.config({path: "./.env"})
+
+const client = new Client({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DB,
+})
+
+client.connect().then(() => console.log("Database connected"))
+
+export default client
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,24 @@
 import express from "express"
 const app = express()
+import cors from "cors"
 import dotenv from "dotenv"
-dotenv.config({path: "./.env"})
+dotenv.config({ path: "./.env" })
+import client from "./db.js"
 
 app.use(express.json())
 app.use(cors())
-app.listen(process.API_PORT, () => console.log(`Server is running on ${process.API_URL}:${process.API_PORT}`))
+app.listen(process.env.API_PORT, () => console.log(`Server is running on ${process.env.API_URL}:${process.env.API_PORT}`))
+
+app.post("/short", async (req, res) => {
+    try {
+        const { originalUrl } = req.body
+        const shortId = Math.random().toString(36).substring(2, 8)
+        const result = await client.query(
+            "INSERT INTO urls (original_url, short_url) VALUES($1, $2) RETURNING short_url", [originalUrl, shortId]
+        )
+        res.status(201).json(result.rows)
+    } catch (err) {
+        console.log(err.message)
+        res.status(500).send("Server error")
+    }
+})
